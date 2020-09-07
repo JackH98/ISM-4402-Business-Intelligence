ffdf4b02e145f41d1f6411c0e98667fa9dd4aa12
JackH98 <70179444+JackH98@users.noreply.github.com>
2020-09-07 03:56:37 -0400

    Add files via upload

---
+++ Your Turn Pg 7.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+Location = "/Users/jackh/Downloads/all_040_in_12.P1.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+import pandas as pd
+Location = "/Users/jackh/Downloads/all_040_in_12.P1.csv"
+df = pd.read_csv(Location)
+
+
+# In[5]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
